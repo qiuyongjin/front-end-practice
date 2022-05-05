0315342a25f67aa3f6a02a90757124cf216ec245
yqiu <qiu5630@163.com>
2022-05-05 16:06:28 +0800

    enhance

--- src/views/code-organization/index.vue
+++ src/views/code-organization/index.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import {count, addCount} from "./index";
+import {ref, watchEffect} from "vue";
 
+const setCount = ref(0)
+const performAsyncOperation = (num: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('performAsyncOperation')
+      resolve(num + 100)
+    }, 1000)
+  })
+}
+const watchEffectStop = watchEffect(() => {
+  setCount.value++
+  console.log('watchEffect:', count.value)
+})
 const handleAdd = () => {
   count.value += 2;
+  // 当count大于10时，停止watchEffect
+  count.value >= 10 && watchEffectStop();
+
 }
 </script>
 
 <template>
   <div class="text-center">
-    <p>{{ count }}</p>
+    <p>{{ count }},set:{{ setCount }}</p>
     <button @click="addCount" class="mr-5">Add</button>
     <button @click="handleAdd">Add 2</button>
   </div>
