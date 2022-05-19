bf2f37b984f60854087a10516b92b2538aad36a6
yqiu <qiu5630@163.com>
2022-05-20 01:13:05 +0800

    enhance

--- src/views/echarts/index.vue
+++ src/views/echarts/index.vue
@@ -23,14 +23,52 @@ const option = reactive({
     data: [5, 20, 36, 10, 10, 20]
   }]
 })
+const option2 = reactive({
+  title: {
+    text: 'Referer of a Website',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        {value: 1048, name: 'Search Engine'},
+        {value: 735, name: 'Direct'},
+        {value: 580, name: 'Email'},
+        {value: 484, name: 'Union Ads'},
+        {value: 300, name: 'Video Ads'}
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+})
 
 let myChart: EChartsType | null = null
+let myChart2: EChartsType | null = null
 
 function init() {
 // 基于准备好的dom，初始化echarts实例
   myChart = echarts.init(document.getElementById('echarts') as HTMLDivElement)
+  myChart2 = echarts.init(document.getElementById('echarts2') as HTMLDivElement)
   // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(option)
+  myChart2.setOption(option2)
 }
 
 const handleAddData = () => {
@@ -50,6 +88,7 @@ onMounted(() => {
     <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
     <button @click="handleAddData">Add Data</button>
     <div id="echarts" style="width: 600px;height:400px;"></div>
+    <div id="echarts2" style="width: 600px;height:400px;"></div>
   </div>
 </template>
 
